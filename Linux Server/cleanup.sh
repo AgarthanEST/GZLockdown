c6fb190b2a286b1f30f7fab7088bc8643e2cae0c
EST <183599137+AgarthanEST@users.noreply.github.com>
2025-09-29 15:08:00 +0000

    Create cleanup.sh

---
+++ Linux Server/cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo rm ~/install.sh
+sudo rm ~/newuser.sh
+sudo rm ~/cleanup.sh
+#sudo rm -rf ~/DIRECTORY_NAME
+#Add directory name to the files above too
+
+echo
+echo "Setup Files Cleaned Successfully"
