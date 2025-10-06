11740eae4ab4c6d5a996b5f9c03a0da0f285797c
EST <183599137+AgarthanEST@users.noreply.github.com>
2025-10-06 17:26:00 +0000

    Fix install.sh
    Minor spelling mistake :/

--- Linux Server/install.sh
+++ Linux Server/install.sh
@@ -16,7 +16,7 @@ clear
 
 read -p "New Username: " desired_user && adduser $desired_user && usermod -aG sudo $desired_user && \
 echo "$desired_user ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$desired_user && \
-cp setup_user.sh /home/$desired_user/ && chown $desired_user:$desired_user /home/$desired_user/setup_user.sh && \
+cp newuser.sh /home/$desired_user/ && chown $desired_user:$desired_user /home/$desired_user/newuser.sh && \
 su - $desired_user -c "bash ~/newuser.sh"
 
 # NOTE: Auto clean at the end! Ensure that the script is in ~/
