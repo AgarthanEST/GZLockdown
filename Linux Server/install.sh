df6b58ff53b70736057706bead939107a669855c
EST <183599137+AgarthanEST@users.noreply.github.com>
2025-10-06 23:28:23 +0000

    Update install.sh
    User Creation Automation

--- Linux Server/install.sh
+++ Linux Server/install.sh
@@ -4,6 +4,23 @@
 # Minimise attack surface with minimal software installs. Limit permissions
 # Do a Y/N on if this is a fresh server on root?
 
+## Customisation Variables
+clear
+read -p "New Username: " desired_user
+
+while true; do
+  read -s -p "Enter password: " Password
+  echo
+  read -s -p "Retype password: " Password2
+  echo
+  if [ "$Password" == "$Password2" ]; then
+    echo "Passwords match."
+    break
+  else
+    echo "Passwords do not match. Please try again."
+  fi
+done
+
 ## Unattended Security Updates
 set -e
 export DEBIAN_FRONTEND=noninteractive
@@ -27,7 +44,12 @@ cp cleanup.sh /home/$desired_user/
 chown $desired_user:$desired_user /home/$desired_user/cleanup.sh
 
 ## Create Non-Root Sudo User
-read -p "New Username: " desired_user && adduser $desired_user && usermod -aG sudo $desired_user && \
+adduser --disabled-password --gecos ",,," "$desired_user"
+echo "$desired_user:$Password" | chpasswd
+
+#adduser $desired_user
+
+usermod -aG sudo $desired_user && \
 echo "$desired_user ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$desired_user && \
 cp newuser.sh /home/$desired_user/ && chown $desired_user:$desired_user /home/$desired_user/newuser.sh && \
 su - $desired_user -c "bash ~/newuser.sh"
