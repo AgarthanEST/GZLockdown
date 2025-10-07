84747392f579342a6aca4f6aaf1c88292b119b75
EST <183599137+AgarthanEST@users.noreply.github.com>
2025-10-07 01:09:45 +0000

    Update cleanup.sh
    Silence and Simplify

--- Linux Server/cleanup.sh
+++ Linux Server/cleanup.sh
@@ -4,10 +4,3 @@
 sudo rm newuser.sh
 sudo rm custom.sh
 sudo rm cleanup.sh
-
-#sudo rm ~/install.sh (When you can figure out priv escalation to clear root)
-#sudo rm -rf ~/DIRECTORY_NAME
-#Add directory name to the files above too
-
-echo
-echo "Setup Files Cleaned Successfully"
