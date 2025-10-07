6ec46ab47b78ee7d7dc4d9942e5a56fd7b3c9e2e
EST <183599137+AgarthanEST@users.noreply.github.com>
2025-10-07 00:37:08 +0000

    Update cleanup.sh

--- Linux Server/cleanup.sh
+++ Linux Server/cleanup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 ## Cleanup
+sudo rm newuser.sh
+sudo rm custom.sh
+sudo rm cleanup.sh
+
 #sudo rm ~/install.sh (When you can figure out priv escalation to clear root)
 #sudo rm -rf ~/DIRECTORY_NAME
 #Add directory name to the files above too
