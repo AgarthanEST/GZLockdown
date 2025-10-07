9d6fdf470c1a4b5b7d9d48c0307eff7faf431f06
EST <183599137+AgarthanEST@users.noreply.github.com>
2025-10-07 00:35:11 +0000

    Update newuser.sh

--- Linux Server/newuser.sh
+++ Linux Server/newuser.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-## Functions
-function yes_or_no {
-    while true; do
-        read -p "$* [y/n]: " yn
-        case $yn in
-            [Yy]*) return 0  ;;  
-            [Nn]*) return  1 ;;
-        esac
-    done
-}
-
 ## Setup
 cd ~
 mkdir ~/.ssh
@@ -32,11 +21,6 @@ sudo ufw status
 ## Scan Optional Scripts
 [ -f "./custom.sh" ] && ./custom.sh
 [ -f "./cleanup.sh" ] && ./cleanup.sh
-
-clear
-echo "CONTEXT: An SSH Keypair allows passwordless login via generating a keyfile for authentication" 
-echo "It is harder to brute force than a password"
-yes_or_no "Are you interested in creating an SSH Keypair?" && echo "TEST SUCCESSS"
 
 # NOTE: Install other tools optionally here.
 
