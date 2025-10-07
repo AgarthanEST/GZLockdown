c510b3a9c4ffc60aa5d1f4aaf560faa63343c3e3
EST <183599137+AgarthanEST@users.noreply.github.com>
2025-10-07 00:34:53 +0000

    Update cleanup.sh

--- Linux Server/cleanup.sh
+++ Linux Server/cleanup.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-## Functions
-
-function yes_or_no {
-    while true; do
-        read -p "$* [y/n]: " yn
-        case $yn in
-            [Yy]*) return 0  ;;  
-            [Nn]*) return  1 ;;
-        esac
-    done
-}
-
 ## Cleanup
-
-yes_or_no "Do you want all related non-root files cleaned?" && sudo rm ~/newuser.sh && sudo rm ~/cleanup.sh
-
 #sudo rm ~/install.sh (When you can figure out priv escalation to clear root)
 #sudo rm -rf ~/DIRECTORY_NAME
 #Add directory name to the files above too
